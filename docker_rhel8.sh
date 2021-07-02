d972eb6c1c5e8998966e5172a089f646bc2d3552
Aleksey <smiledevops@gmail.com>
2021-07-02 10:41:45 -0700

    Add docker install config rhel8

---
+++ docker_rhel8.sh
@@ -0,0 +1,11 @@
+sudo dnf update && sudo dnf -y upgrade
+
+sudo dnf -y install yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf update
+
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker && sudo systemctl enable docker
